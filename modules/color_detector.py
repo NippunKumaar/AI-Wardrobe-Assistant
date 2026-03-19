867e6d9fbd8ea1d5b8da699ea8dce5cf52161982
Nippun Kumaar <nippun05@gmail.com>
2026-03-19 21:16:56 +0530

    Included HEIC format for input image, added RGB format for color identifier for brown color and fine tuned HSV rule.

--- modules/color_detector.py
+++ modules/color_detector.py
@@ -60,41 +60,81 @@ def get_dominant_color(image: Image.Image):
     labels = kmeans.labels_
     clusters = kmeans.cluster_centers_
 
-    # count cluster sizes
     counts = np.bincount(labels)
 
-    # pick largest cluster (most dominant area)
-    dominant_rgb = clusters[np.argmax(counts)]
+    # get top 2 clusters
+    top_indices = counts.argsort()[-2:][::-1]
+
+    candidate_clusters = clusters[top_indices]
+
+    dominant_rgb = select_best_cluster(candidate_clusters)
 
     hsv = rgb_to_hsv(dominant_rgb.reshape(1,1,3)).reshape(3)
     h, s, v = hsv
+    r, g, b = dominant_rgb
 
-    return classify_color(h, s, v)
+    return classify_color(h, s, v, r, g, b)
 
 
-def classify_color(h, s, v):
+def classify_color(h, s, v, r,g, b):
 
-    # handle neutral colors first
+    if r > 80 and g > 40 and b < 100 and r > g > b:
+        return "brown"
+
+    # 1. Very dark → black
+    if v < 0.2:
+        return "black"
+
+    # 2. Light blue fix (critical)
+    if 180 <= h <= 260 and s < 0.4 and v > 0.7:
+        return "blue"
+
+    # 3. Neutral colors
     if s < 0.25:
-        if v > 0.8:
+        if v > 0.85:
             return "white"
-        elif v > 0.4:
+        elif v > 0.35:
             return "grey"
         else:
             return "black"
 
-    # hue-based classification
-    if h < 15 or h >= 345:
-        return "red"
-    elif h < 35:
-        return "orange"
-    elif h < 55:
-        return "yellow"
-    elif h < 85:
-        return "green"
-    elif h < 140:
+    
+    # Improved brown detection
+    if 10 < h < 50 and v < 0.85 and s > 0.35:
+        return "brown"
+
+    # 5. Blue
+    if 180 <= h <= 260:
         return "blue"
-    elif h < 170:
-        return "purple"
-    else:
+
+    # 6. Red / Pink
+    if h < 10 or h > 340:
+        return "red"
+    if 300 <= h <= 340:
         return "pink"
+
+    # beige / cream detection (optional refinement)
+    if 20 < h < 50 and s < 0.35 and v > 0.75:
+        return "beige"
+
+    # 7. Remaining colors
+    if 10 <= h < 45:
+        return "orange"
+    elif 45 <= h < 70:
+        return "yellow"
+    elif 70 <= h < 160:
+        return "green"
+    elif 260 < h < 300:
+        return "purple"
+
+    return "unknown"
+
+def select_best_cluster(clusters):
+    # choose cluster with highest red dominance for brown-like colors
+    scores = []
+
+    for r, g, b in clusters:
+        score = r - b  # brown has strong red dominance over blue
+        scores.append(score)
+
+    return clusters[np.argmax(scores)]
